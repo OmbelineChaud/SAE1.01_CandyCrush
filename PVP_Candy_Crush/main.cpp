83b907cc31f1b66da6b9166247dfb0a595efcd7f
OmbelineChaud <ombeline.chaud@etu.univ-amu.fr>
2025-12-16 20:18:04 +0100

    unsigned devient int main.cpp

--- PVP_Candy_Crush/main.cpp
+++ PVP_Candy_Crush/main.cpp
@@ -150,7 +150,7 @@ bool atLeastThreeInARow(const mat &grid, maPosition &pos, unsigned &howMany) {
 }
 
 void removalInColumn(mat &grid, const maPosition &pos, unsigned &howMany) {
-    unsigned col = pos.abs;
+    int col = pos.abs;
     int start = pos.ord;
     int end = start + (int)howMany-1;//int entre parenthèse met howManny à une valeur numérique int.
     //on met les éléments à supprimer à valeur KImpossible
@@ -170,7 +170,7 @@ void removalInColumn(mat &grid, const maPosition &pos, unsigned &howMany) {
 }
 
 void removalInRow(mat &grid, const maPosition &pos, unsigned &howMany) {
-    unsigned ligne = pos.ord; //similaire à removalInColumn
+    int ligne = pos.ord; //similaire à removalInColumn
     int start = pos.abs; 
     int end = start + (int)howMany-1; //int entre parenthèse met howManny à une valeur numérique int.
     //on met les éléments à supprimer à valeur KImpossible
