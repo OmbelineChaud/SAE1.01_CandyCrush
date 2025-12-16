fb74d5777e44294f858c5dbad5ea49d85a698898
OmbelineChaud <ombeline.chaud@etu.univ-amu.fr>
2025-12-16 19:45:25 +0100

    retirer la couleur noir, on voit pas le chiffre sur terminal blanc main.cpp

--- PVP_Candy_Crush/main.cpp
+++ PVP_Candy_Crush/main.cpp
@@ -67,7 +67,7 @@ void displayGrid(const mat &grid, const vector<unsigned> &colors) {
                 cout << "   ";
             }
             else {
-                couleur(colors[grid[i][j] - 1] + 10); 
+                couleur(colors[grid[i][j]] + 10); 
                 cout << " " << grid[i][j] << " ";
                 couleur(KReset);
             }
