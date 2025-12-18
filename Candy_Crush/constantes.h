ca7ab14e557290e3fccc86b8f67d428d6e1507ae
OmbelineChaud <ombeline.chaud@etu.univ-amu.fr>
2025-12-18 17:29:24 +0100

    Add files via upload
    au moins une segmentation

---
+++ Candy_Crush/constantes.h
@@ -0,0 +1,19 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+#pragma once
+
+//couleurs
+constexpr unsigned KReset      = 0;
+constexpr unsigned KNoir       = 30;
+constexpr unsigned KRouge      = 31;
+constexpr unsigned KVert       = 32;
+constexpr unsigned KJaune      = 33;
+constexpr unsigned KBleu       = 34;
+constexpr unsigned KMAgenta    = 35;
+constexpr unsigned KCyan       = 36;
+//vaeur pour case vide
+constexpr unsigned KImpossible = 0;
+//valeur maximale de bonbon
+constexpr int      KNbCandies  = 6;
+
+#endif // CONSTANTES_H
